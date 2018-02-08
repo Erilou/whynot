9c975e722e2fc17e0bba9117c3c218b81d863a0c
Erilou <eriliapro@gmail.com>
2018-02-08 09:27:28 +0100

    Connection to the database

--- WhyNotSurv/src/fr/whynot/core/Core.java
+++ WhyNotSurv/src/fr/whynot/core/Core.java
@@ -2,13 +2,17 @@ package fr.whynot.core;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import fr.whynot.core.database.SQL;
 import fr.whynot.core.manager.CommandsManager;
 import fr.whynot.core.manager.EventsManager;
 
 public class Core extends JavaPlugin {
 	
+	public SQL sql;
+	
 	public void onEnable() {
 		System.out.println("Plugin Allumé");
+		sql.connect("62.210.119.133:25580", "WhyNotSurvive", 3306, "Whynot", "4cZWSYUUocAfKO5x");
 		new EventsManager().registers(this);
 		new CommandsManager().registers(this);
 	}
