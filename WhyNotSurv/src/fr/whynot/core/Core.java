46ca07b6e29a96b1be5783c589f0451919499303
Erilou <eriliapro@gmail.com>
2018-02-08 09:34:28 +0100

    Initialisation of instance to main class

--- WhyNotSurv/src/fr/whynot/core/Core.java
+++ WhyNotSurv/src/fr/whynot/core/Core.java
@@ -5,11 +5,17 @@ import org.bukkit.plugin.java.JavaPlugin;
 import fr.whynot.core.database.SQL;
 import fr.whynot.core.manager.CommandsManager;
 import fr.whynot.core.manager.EventsManager;
+import fr.whynot.core.rank.Rank;
 
 public class Core extends JavaPlugin {
 	
 	public SQL sql;
+	public Rank rank;
+	private static Core instance;
 	
+	public static Core getInstance() {
+		return instance;
+	}
 	public void onEnable() {
 		System.out.println("Plugin Allumé");
 		sql.connect("62.210.119.133:25580", "WhyNotSurvive", 3306, "Whynot", "4cZWSYUUocAfKO5x");
